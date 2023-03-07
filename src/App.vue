ea919c90602ca1cd29fa82cadbb41c2b0a6a3130
Barbi Torres <barbi.mtorres@gmail.com>
2023-03-07 16:47:57 +0100

    c5-dinamicComponents

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import { defineAsyncComponent } from "vue";
+
+const HelloWorld = defineAsyncComponent(() =>
+  import("./components/HelloWorld.vue")
+);
 
 export default {
   name: "App",
